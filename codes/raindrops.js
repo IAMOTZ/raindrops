edb645ddb7b75c9a0368b1dc3daf6ecab0147cd6
tunmise <ogunniyitunmise@gmail.com>
2017-10-23 14:44:42 +0100

    Refractor the main program codes

--- codes/raindrops.js
+++ codes/raindrops.js
@@ -1,6 +1,42 @@
-let primeFactorsOf = (number) => {
-    let factors = [], test = 2, result;
 
+let raindropSpeak = (input) => {
+    // Verify inputs
+    if(typeof(input) != 'number') {
+        return undefined;
+    }
+    if(input <= 0) {
+        return undefined;
+    }
+
+    let primeFactors = primeFactorsOf(input);
+    let result = '';
+
+    if(primeFactors.indexOf(3) !== -1) {
+        // If there is a 3 in the facotrs.
+        result += 'Pling'
+    }
+
+    if(primeFactors.indexOf(5) !== -1) {
+        // If there is a 5 in the facotrs.
+        result += 'Plang'
+    }
+
+    if(primeFactors.indexOf(7) !== -1) {
+        // If there is a 7 in the facotrs.        
+        result += 'Plong'
+    }
+
+    if(result == '') {
+        // If there the result does not contain 3,5 or 7      
+        return input;
+    } 
+    
+    return result;
+}
+
+let primeFactorsOf = (number) => {
+    // Get the prime factors of a number.
+    let factors = [], test = 2, result;
     while(number !== 1) {
         if(number % test === 0) {
             factors.push(test);
@@ -15,35 +51,6 @@ let primeFactorsOf = (number) => {
     return result;
 }
 
-let raindropSpeak = (input) => {
-    // Verify inputs
-    if(typeof(input) != 'number') {
-        return undefined;
-    }
-
-    if(input <= 0) {
-        return undefined;
-    }
-
-    let primeFactors = primeFactorsOf(input);
-    let result = '';
-
-    if(primeFactors.indexOf(3) !== -1) {
-        result += 'Pling'
-    }
-
-    if(primeFactors.indexOf(5) !== -1) {
-        result += 'Plang'
-    }
-
-    if(primeFactors.indexOf(7) !== -1) {
-        result += 'Plong'
-    }
-    if(result == '') {
-        return input;
-    } 
-    
-    return result;
-}
+console.log(raindropSpeak(1775));
 
 module.exports = raindropSpeak;
